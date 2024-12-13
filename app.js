55d2612fc7329ede6e401259da608015a9e0b953
GM <t1mukgi@gmail.com>
2024-12-12 22:47:11 -0500

    Calc Completed

---
+++ app.js
@@ -0,0 +1,16 @@
+const result = document.getElementById("result");
+
+function appendToResult(input){
+result.value+= input;
+}
+function calculate(){
+    try {
+        result.value  =  eval(result.value)
+    } catch (error) {
+        result.value  =  "Error"
+    }
+}
+
+function clearResult(){
+result.value =""
+}
